8f1f25a4f2b0ef680bf73645458e85f235563837
Robert Castillo <robert.castillo@gmx.at>
2020-11-07 16:54:16 +0100

    database

---
+++ mail_delivery (1).sql
@@ -0,0 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 07. Nov 2020 um 16:53
+-- Server-Version: 10.4.14-MariaDB
+-- PHP-Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `mail_delivery`
+--
+CREATE DATABASE IF NOT EXISTS `mail_delivery` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `mail_delivery`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `address`
+--
+
+CREATE TABLE `address` (
+  `address_id` int(11) NOT NULL,
+  `street` varchar(55) DEFAULT NULL,
+  `city` varchar(55) DEFAULT NULL,
+  `tel_number` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
+  `first_name` varchar(55) DEFAULT NULL,
+  `last_name` varchar(55) DEFAULT NULL,
+  `fk_address_id` int(55) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `employees`
+--
+
+CREATE TABLE `employees` (
+  `employees_id` int(11) NOT NULL,
+  `first_name` varchar(55) DEFAULT NULL,
+  `last_name` varchar(55) DEFAULT NULL,
+  `department` varchar(55) DEFAULT NULL,
+  `fk_address_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `mail_station`
+--
+
+CREATE TABLE `mail_station` (
+  `mail_station_id` int(11) NOT NULL,
+  `fk_address_id` int(11) DEFAULT NULL,
+  `fk_customer_id` int(11) DEFAULT NULL,
+  `fk_employees_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`address_id`);
+
+--
+-- Indizes für die Tabelle `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD KEY `fk_address_id` (`fk_address_id`);
+
+--
+-- Indizes für die Tabelle `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employees_id`),
+  ADD KEY `fk_address_id` (`fk_address_id`);
+
+--
+-- Indizes für die Tabelle `mail_station`
+--
+ALTER TABLE `mail_station`
+  ADD PRIMARY KEY (`mail_station_id`),
+  ADD KEY `fk_address_id` (`fk_address_id`),
+  ADD KEY `fk_customer_id` (`fk_customer_id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `address`
+--
+ALTER TABLE `address`
+  MODIFY `address_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employees_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `mail_station`
+--
+ALTER TABLE `mail_station`
+  MODIFY `mail_station_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `customer_ibfk_1` FOREIGN KEY (`fk_address_id`) REFERENCES `address` (`address_id`);
+
+--
+-- Constraints der Tabelle `employees`
+--
+ALTER TABLE `employees`
+  ADD CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`fk_address_id`) REFERENCES `address` (`address_id`);
+
+--
+-- Constraints der Tabelle `mail_station`
+--
+ALTER TABLE `mail_station`
+  ADD CONSTRAINT `mail_station_ibfk_1` FOREIGN KEY (`fk_address_id`) REFERENCES `address` (`address_id`),
+  ADD CONSTRAINT `mail_station_ibfk_2` FOREIGN KEY (`fk_customer_id`) REFERENCES `customer` (`customer_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
