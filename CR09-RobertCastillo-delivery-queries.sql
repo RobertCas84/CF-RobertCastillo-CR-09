e8835c8bc861d41af964bd5773dcba54da6861af
Robert Castillo <robert.castillo@gmx.at>
2020-11-12 02:09:43 +0100

    queries update

--- CR09-RobertCastillo-delivery-queries.sql
+++ CR09-RobertCastillo-delivery-queries.sql
@@ -6,3 +6,17 @@ customer.fk_address_id=address.address_id
 JOIN delivering_route
 ON 
 delivering_route.fk_customer_id=customer.customer_id
+
+/*left JOIN*/
+SELECT customer.first_name, customer.last_name,customer.fk_address_id
+FROM customer
+LEFT JOIN address on 'fk_customer' = address_id;
+/* Join*/
+SELECT employees.first_name, employees.last_name,employees.department
+FROM employees
+JOIN address 
+ON address.fk_employees_id = employees_id;
+/* right join*/
+SELECT customer.first_name, customer.last_name, customer.fk_address_id
+FROM `customer`
+RIGHT JOIN mail_station ON mail_station.fk_customer_id = customer_id;
